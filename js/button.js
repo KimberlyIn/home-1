92e6897bd0c327ec8d73bfe422beb65fed01966a
KimberlyIn <51989121+KimberlyIn@users.noreply.github.com>
2021-02-04 16:07:12 +0800

    Add files via upload

--- js/button.js
+++ js/button.js
@@ -1,11 +1,13 @@
-$('.option ul li button.shopping-bag').click{function() {
-    $(this).children('i').toggleClass('icon-red');
-});
-
-$('.option ul li button.heart-empty').click{function() {
-    $(this).children('i').toggleClass('icon-heart icon-red');
-});
-
-$('.option ul li button.eye').click{function() {
-    $(this).children('i').toggleClass('icon-red');
+$(document).ready(function(){
+    $('.option ul li button.shopping-bag').click(function() {
+        $(this).children('i').toggleClass('icon-red');
+    });
+    
+    $('.option ul li button.heart-empty').click(function() {
+        $(this).children('i').toggleClass('icon-heart icon-red');
+    });
+    
+    $('.option ul li button.eye').click(function() {
+        $(this).children('i').toggleClass('icon-red');
+    });
 });
